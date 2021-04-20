267a53316053aee6f53001b98ee5a1b7883e0a64
mwyd <wydrzanin@gmail.com>
2021-04-20 18:47:34 +0200

    Initial commit

---
+++ database/migrations/2021_04_20_163521_create_steam_market_csgo_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSteamMarketCsgoItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('steam_market_csgo_items', function (Blueprint $table) {
+            $table->string('hash_name')->index();
+            $table->mediumInteger('volume');
+            $table->mediumInteger('price');
+            $table->string('icon');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('steam_market_csgo_items');
+    }
+}
