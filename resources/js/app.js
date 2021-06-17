1ca097644d654179cc51bb288eecbbd34dd27ea7
mwyd <wydrzanin@gmail.com>
2021-06-17 12:52:34 +0200

    Add vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 require('./bootstrap');
+
+window.Vue = require('vue')
+
+import App from './components/App'
+
+const app = Vue.createApp(App)
+app.mount('#app')
---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>Hello</div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    mounted() {
+        console.log(window)
+    }
+}
+</script>
+
+<style>
+
+</style>
