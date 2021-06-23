c174bb11ea58366812715794e6b5675d3df98043
mwyd <wydrzanin@gmail.com>
2021-06-23 13:30:41 +0200

    Rename provider: RestApiProvider.php -> ApiResponseProvider.php

--- app/Providers/ApiResponseProvider.php
+++ app/Providers/ApiResponseProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Response;
 
-class RestApiProvider extends ServiceProvider
+class ApiResponseProvider extends ServiceProvider
 {
     /**
      * Register services.
