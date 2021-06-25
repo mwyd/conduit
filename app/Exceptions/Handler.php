4ad005486e9dec7a598944be1a3d929ddf1ddea0
mwyd <wydrzanin@gmail.com>
2021-06-25 12:42:45 +0200

    Add message

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,12 +2,13 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Access\AuthorizationException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -52,11 +53,12 @@ class Handler extends ExceptionHandler
             $message = match($e::class) {
                 ValidationException::class      => ['wrong_params', 422],
                 ModelNotFoundException::class   => ['not_found', 404],
-                AuthenticationException::class  => ['unauthorized', 401],
+                AuthenticationException::class  => ['forbidden', 403],
+                AuthorizationException::class   => ['unauthorized', 401],
                 HttpException::class            => [$e->getMessage(), $e->getStatusCode()],
                 default                         => [config('app.debug') ? $e->getMessage() : 'internal_error', 500]
             };
-
+            
             $view = response()->apiFail(...$message);
         }
 
