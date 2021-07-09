a52d056f373d008984442dae43c21e441496d3e5
mwyd <wydrzanin@gmail.com>
2021-07-09 23:43:32 +0200

    Frontend: make error logs less detailed

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -282,7 +282,7 @@ export default {
                 }
             }
             catch(err) {
-                console.log(err)
+                console.log(err?.message)
             }
 
             this.contentLoaded = true
