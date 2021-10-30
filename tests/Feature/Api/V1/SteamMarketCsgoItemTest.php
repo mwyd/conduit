e075cf748b42a8e61e12c8df7d0d9d1c7060cd9f
mwyd <wydrzanin@gmail.com>
2021-10-30 16:30:26 +0200

    Add unauthorized cases to steam market csgo item test

--- tests/Feature/Api/V1/SteamMarketCsgoItemTest.php
+++ tests/Feature/Api/V1/SteamMarketCsgoItemTest.php
@@ -60,19 +60,11 @@ class SteamMarketCsgoItemTest extends TestCase
             ]);
     }
 
-    public function test_authorized_user_can_create_item()
+    /**
+     * @dataProvider postItemDataProvider
+     */
+    public function test_authorized_user_can_create_item($formData)
     {
-        $formData = [
-            'hash_name'     => $this->faker()->words(3, true),
-            'volume'        => $this->faker()->numberBetween(0, 100),
-            'price'         => $this->faker()->randomFloat(2, 0 ,100),
-            'icon'          => Str::random(64),
-            'icon_large'    => Str::random(64),
-            'name_color'    => $this->faker()->hexColor(),
-            'type'          => $this->faker()->words(3, true),
-            'collection'    => null
-        ];
-
         Sanctum::actingAs(
             User::factory()->create(),
             ['api:post']
@@ -85,6 +77,25 @@ class SteamMarketCsgoItemTest extends TestCase
             ->assertJson([
                 'success'   => true,
                 'data'      => $formData
+            ]);
+    }
+
+    /**
+     * @dataProvider postItemDataProvider
+     */
+    public function test_unauthorized_user_cannot_create_item($formData)
+    {
+        Sanctum::actingAs(
+            User::factory()->create()
+        );
+
+        $response = $this->json('POST', '/api/v1/steam-market-csgo-items', $formData);
+
+        $response
+            ->assertStatus(401)
+            ->assertJson([
+                'success'       => false,
+                'error_message' => 'unauthorized'
             ]);
     }
 
@@ -112,6 +123,29 @@ class SteamMarketCsgoItemTest extends TestCase
             ]);
     }
 
+    public function test_unauthorized_user_cannot_update_item()
+    {
+        $item = SteamMarketCsgoItem::factory()->create();
+
+        $formData = [
+            'volume'    => $this->faker()->numberBetween(0, 100),
+            'price'     => $this->faker()->randomFloat(2, 0 ,100)
+        ];
+
+        Sanctum::actingAs(
+            User::factory()->create()
+        );
+
+        $response = $this->json('PUT', '/api/v1/steam-market-csgo-items/' . $item->hash_name, $formData);
+
+        $response
+            ->assertStatus(401)
+            ->assertJson([
+                'success'       => false,
+                'error_message' => 'unauthorized'
+            ]);
+    }
+
     public function test_authorized_user_can_delete_item()
     {
         $item = SteamMarketCsgoItem::factory()->create();
@@ -129,5 +163,41 @@ class SteamMarketCsgoItemTest extends TestCase
                 'success'   => true,
                 'data'      => $item->toArray()
             ]);
+    }
+
+    public function test_unauthorized_user_cannot_delete_item()
+    {
+        $item = SteamMarketCsgoItem::factory()->create();
+
+        Sanctum::actingAs(
+            User::factory()->create()
+        );
+
+        $response = $this->json('DELETE', '/api/v1/steam-market-csgo-items/' . $item->hash_name);
+
+        $response
+            ->assertStatus(401)
+            ->assertJson([
+                'success'       => false,
+                'error_message' => 'unauthorized'
+            ]);
+    }
+
+    public function postItemDataProvider()
+    {
+        return [
+            [
+                [
+                    'hash_name'     => 'AK-47 | Asiimov (Field-Tested)',
+                    'volume'        => 100,
+                    'price'         => 77.77,
+                    'icon'          => '-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot7HxfDhjxszJemkV092lnYmGmOHLPr7Vn35cppQiiOuQpoml3wW18xdkNTjxd9CQdwM_ZlrT-lW_kLzu0560vp-azXJ9-n51Q5-Fea0',
+                    'icon_large'    => '-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot7HxfDhjxszJemkV092lnYmGmOHLPr7Vn35c18lwmO7Eu92milbl-BZsZGiiLNKdJFc8Mg7V_1S_xuzshZK97c_In3pruCJx4X_D30vgyZM--n4',
+                    'name_color'    => '#D2D2D2',
+                    'type'          => 'Covert Rifle',
+                    'collection'    => 'The Danger Zone Collection'
+                ]
+            ]
+        ];
     }
 }
