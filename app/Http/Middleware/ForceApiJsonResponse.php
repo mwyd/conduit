d257347b1da75e6081fcbfe3f6c5868d3e12b483
mwyd <wydrzanin@gmail.com>
2021-10-31 12:13:03 +0100

    Sugar update

--- app/Http/Middleware/ForceApiJsonResponse.php
+++ app/Http/Middleware/ForceApiJsonResponse.php
@@ -17,6 +17,7 @@ class ForceApiJsonResponse
     public function handle(Request $request, Closure $next)
     {
         $request->headers->set('Accept', 'application/json');
+
         return $next($request);
     }
 }
